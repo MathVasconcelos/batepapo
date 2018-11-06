beb6185adf314be78199ebe093b904410addcaf4
aluno <aluno>
2018-11-06 10:18:04 -0200

    primeiro commit

---
+++ Mensagens.php
@@ -0,0 +1,43 @@
+<?php
+class Mensagens {
+
+	public function __construct() {
+  	$this->conectar();
+}
+
+    protected $conexao;
+
+    public function conectar()
+    {
+        $this->conexao = new PDO("mysql:host=localhost;dbname=batepapo", "root", "");
+    }
+
+    public function getTodasMensagens()
+    {
+        $comando = $this->conexao->prepare("SELECT * FROM mensagem");
+        $comando->execute();
+
+        return $comando;
+    }
+public function enviar($dados)
+{
+    $comando = $this->conexao->prepare(
+        "INSERT INTO mensagem (
+            remetente,
+            texto
+        ) VALUES (
+            :remetente,
+            :texto
+        )"
+    );
+
+    $dados = [
+        ':remetente' => $dados['remetente'],
+        ':texto' => $dados['texto']
+    ];
+
+    $comando->execute($dados);
+    return $this->conexao->lastInsertId();
+	}
+}
+?>
---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+var_dump($_POST);
+?>
+<?php
+
+require 'Mensagens.php';
+
+if (isset($_POST) && sizeof($_POST) > 0) {
+  $dados = new Mensagens();
+  $id = $dados->enviar($_POST);
+  echo $id;
+}
+?>
